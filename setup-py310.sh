4a636b508010196ea0f980c7f5874f1aed61ec35
Paolo Di Francesco <frpaolo@amazon.at>
2026-04-30 08:46:28 +0200

    Fix conda activation in setup-py310.sh for non-interactive shells

--- setup-py310.sh
+++ setup-py310.sh
@@ -2,7 +2,8 @@
 set -e
 
 conda create -n py310 python=3.10 -y
-source activate py310
+eval "$(conda shell.bash hook)"
+conda activate py310
 pip install -q ipykernel
 python -m ipykernel install --name py310 --display-name "Python 3.10" --prefix=/opt/conda
 pip install pickleshare ipywidgets
