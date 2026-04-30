0e486b63ae670b1dd30aef14f1ebee755e264c81
Paolo Di Francesco <frpaolo@amazon.at>
2026-04-30 09:42:20 +0200

    Add pip explicitly to conda create in setup-py310.sh

--- setup-py310.sh
+++ setup-py310.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 set -e
 
-conda create -n py310 python=3.10 ipykernel pickleshare ipywidgets -y
-conda run -n py310 python -m ipykernel install --name py310 --display-name "Python 3.10" --prefix=/opt/conda
+conda create -n py310 python=3.10 pip -y
+source activate py310
+pip install -q ipykernel
+python -m ipykernel install --name py310 --display-name "Python 3.10" --prefix=/opt/conda
+pip install pickleshare ipywidgets
