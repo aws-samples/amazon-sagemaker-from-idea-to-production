f804253b5402ddbd1cbc0378a57746d6c9a05af2
Paolo Di Francesco <frpaolo@amazon.at>
2026-07-10 12:19:09 +0000

    replace pip with uv

--- setup-py312.sh
+++ setup-py312.sh
@@ -3,6 +3,7 @@ set -e
 
 conda create -n py312 python=3.12 pip -y
 source activate py312
-pip install -q ipykernel
+pip install -q uv
+uv pip install --system -q ipykernel
 python -m ipykernel install --name py312 --display-name "Python 3.12" --prefix=/opt/conda
-pip install pickleshare ipywidgets
+uv pip install --system pickleshare ipywidgets
